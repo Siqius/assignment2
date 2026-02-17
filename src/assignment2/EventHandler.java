c98ec31916a59cd5f4216c1183a4a4cb127f57f9
Siqius <Real.FBAB@gmail.com>
2026-02-17 14:45:34 +0100

    sync

--- src/assignment2/EventHandler.java
+++ src/assignment2/EventHandler.java
@@ -5,10 +5,17 @@ import java.awt.event.ActionListener;
 
 public class EventHandler implements ActionListener {
     private final GUI gui;
+    private final int taskID;
     public void actionPerformed(ActionEvent event) {
         switch(event.getActionCommand()) {
-            case "addTask" -> {
-                gui.displayError("hello");
+            case "addHomeTask" -> {
+                gui.createTask("home task");
+            }
+            case "addStudyTask" -> {
+                gui.createTask("study task");
+            }
+            case "addExerciseTask" -> {
+                gui.displayError("hello exercise!");
             }
             default -> {
                 gui.displayError("Event not bound to an action.");
@@ -18,5 +25,10 @@ public class EventHandler implements ActionListener {
 
     EventHandler(GUI gui) {
         this.gui = gui;
+        this.taskID = -1;
+    }
+    EventHandler(GUI gui, int taskID) {
+        this.gui = gui;
+        this.taskID = taskID;
     }
 }
--- src/assignment2/GUI.java
+++ src/assignment2/GUI.java
@@ -1,5 +1,7 @@
 package assignment2;
 
+import se.his.it401g.todo.HomeTask;
+import se.his.it401g.todo.StudyTask;
 import se.his.it401g.todo.Task;
 
 import javax.swing.*;
@@ -8,13 +10,12 @@ import java.util.List;
 
 public class GUI {
     private final JFrame window;
+    private final JPanel headerPanel;
+    private final JPanel addTaskPanel;
     private final JPanel taskPanel;
-    private final JButton addHomeTask;
-    private final JButton addStudyTask;
-    private final JButton addExerciseTask;
     private final EventHandler eventHandler;
     private final JPanel errorDisplay;
-    private List<Task> tasks;
+    private List<HomeTask> tasks;
     public GUI() {
         eventHandler = new EventHandler(this);
         GridBagConstraints c = new GridBagConstraints();
@@ -27,6 +28,106 @@ public class GUI {
         window.setLocation(500, 200);
         window.getContentPane().setBackground(new Color(40, 40, 40));
 
+        headerPanel = getHeaderPanel();
+
+        addTaskPanel = getAddTaskPanel();
+
+        taskPanel = new JPanel();
+        taskPanel.setLayout(new BoxLayout(taskPanel, BoxLayout.Y_AXIS));
+        taskPanel.setBackground(new Color(151, 76, 76));
+        taskPanel.setVisible(true);
+
+        errorDisplay = getErrorDisplayPanel();
+
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1.0;
+        c.weighty = 0.05;
+        c.anchor = GridBagConstraints.NORTH;
+
+        c.gridx = 0;
+        c.gridy = 0;
+        window.add(headerPanel, c);
+        c.gridx = 0;
+        c.gridy = 2;
+        c.weighty = 0.1;
+        window.add(addTaskPanel, c);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.weighty = 0.8;
+        window.add(taskPanel, c);
+        c.gridx = 0;
+        c.gridy = 3;
+        c.weighty = 0.05;
+        window.add(errorDisplay, c);
+        window.setVisible(true);
+
+    }
+
+    public void createTask(String taskType) {
+        switch(taskType) {
+            case("home task") -> {
+                HomeTask homeTask = new HomeTask();
+                taskPanel.add(homeTask, BorderLayout.NORTH);
+                taskPanel.revalidate();
+                taskPanel.repaint();
+            }
+            case("study task") -> {
+                StudyTask studyTask = new StudyTask();
+                taskPanel.add(studyTask, BorderLayout.NORTH);
+                taskPanel.revalidate();
+                taskPanel.repaint();
+            }
+            case("exercise task") -> {
+
+            }
+        }
+    }
+
+    private JPanel getErrorDisplayPanel() {
+        JPanel errorDisplay = new JPanel();
+        errorDisplay.setAlignmentY(Component.CENTER_ALIGNMENT);
+        errorDisplay.setBackground(Color.RED);
+        errorDisplay.setVisible(false);
+
+        JLabel errorDisplayText = new JLabel("none", JLabel.CENTER);
+        errorDisplayText.setFont(new Font("Arial", Font.PLAIN, 24));
+        errorDisplayText.setForeground(Color.BLACK);
+
+        errorDisplay.add(errorDisplayText);
+
+        return errorDisplay;
+    }
+
+    private JPanel getAddTaskPanel() {
+        JPanel addTaskPanel  = new JPanel();
+        addTaskPanel.setLayout(new BoxLayout(addTaskPanel, BoxLayout.X_AXIS));
+
+        JButton addHomeTask = new JButton();
+        addHomeTask.setText("Add home task");
+        addHomeTask.setBackground(new Color(255,255,255));
+        addHomeTask.setActionCommand("addHomeTask");
+        addHomeTask.addActionListener(eventHandler);
+
+        JButton addStudyTask = new JButton();
+        addStudyTask.setText("Add study task");
+        addStudyTask.setBackground(new Color(255,255,255));
+        addStudyTask.setActionCommand("addStudyTask");
+        addStudyTask.addActionListener(eventHandler);
+
+        JButton addExerciseTask = new  JButton();
+        addExerciseTask.setText("Add exercise task");
+        addExerciseTask.setBackground(new Color(255,255,255));
+        addExerciseTask.setActionCommand("addExerciseTask");
+        addExerciseTask.addActionListener(eventHandler);
+
+        addTaskPanel.add(addHomeTask);
+        addTaskPanel.add(addStudyTask);
+        addTaskPanel.add(addExerciseTask);
+
+        return addTaskPanel;
+    }
+
+    private JPanel getHeaderPanel() {
         JPanel headerPanel = new JPanel();
         headerPanel.setAlignmentY(Component.TOP_ALIGNMENT);
         headerPanel.setLayout(new BoxLayout(headerPanel, BoxLayout.X_AXIS));
@@ -52,69 +153,12 @@ public class GUI {
         headerPanel.add(sortSelector);
         headerPanel.add(ascending);
         headerPanel.add(descending);
-
-        JPanel addTasksPanel  = new JPanel();
-        addTasksPanel.setLayout(new BoxLayout(addTasksPanel, BoxLayout.X_AXIS));
-
-        addHomeTask = new JButton();
-        addHomeTask.setText("Add home task");
-        addHomeTask.setBackground(new Color(255,255,255));
-        addHomeTask.setActionCommand("addHomeTask");
-        addHomeTask.addActionListener(eventHandler);
-
-        addStudyTask = new JButton();
-        addStudyTask.setText("Add study task");
-        addStudyTask.setBackground(new Color(255,255,255));
-        addStudyTask.setActionCommand("addStudyTask");
-        addStudyTask.addActionListener(eventHandler);
-
-        addExerciseTask = new  JButton();
-        addExerciseTask.setText("Add exercise task");
-        addExerciseTask.setBackground(new Color(255,255,255));
-        addExerciseTask.setActionCommand("addExerciseTask");
-        addExerciseTask.addActionListener(eventHandler);
-
-        taskPanel = new JPanel();
-        taskPanel.setBackground(new Color(151, 76, 76));
-        taskPanel.setVisible(true);
-
-        errorDisplay = new JPanel();
-        errorDisplay.setAlignmentY(Component.CENTER_ALIGNMENT);
-        errorDisplay.setBackground(Color.RED);
-        errorDisplay.setVisible(false);
-
-        JLabel errorDisplayText = new JLabel("none", JLabel.CENTER);
-        errorDisplayText.setFont(new Font("Arial", Font.PLAIN, 24));
-        errorDisplayText.setForeground(Color.BLACK);
-
-        errorDisplay.add(errorDisplayText);
-
-        JPanel taskPanel = new JPanel(new FlowLayout());
-
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1.0;
-        c.weighty = 0.05;
-        c.anchor = GridBagConstraints.NORTH;
-
-        c.gridx = 0;
-        c.gridy = 0;
-        window.add(headerPanel, c);
-        c.gridx = 0;
-        c.gridy = 1;
-        c.weighty = 0.8;
-        window.add(taskPanel, c);
-        c.gridx = 0;
-        c.gridy = 2;
-        c.weighty = 0.1;
-        window.add(addTasksPanel, c);
-        c.gridx = 0;
-        c.gridy = 3;
-        c.weighty = 0.05;
-        window.add(errorDisplay, c);
-        window.setVisible(true);
-
+        return headerPanel;
     }
 
+    public void hideError() {
+        errorDisplay.setVisible(false);
+    }
 
     public void displayError(String error) {
         System.out.println("Error: " + error);
